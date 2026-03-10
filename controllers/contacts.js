c2012162e9d2a17edbd6df071cae09f9ded8db41
Thomas Garcia <tjg9265@yahoo.com>
2026-03-09 19:35:44 -0600

    update week 2

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -27,8 +27,72 @@ const getSingle = async (req, res) => {
         res.status(500).json({ message: error.message});
     }
 };
+const createContact = async(req, res) => {
+    try{
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+
+        const response = await db.getDb().collection("contacts").insertOne(contact);
+
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+const updateContact = async (req, res) => {
+    try {
+        const contactId = new ObjectId(req.params.id);
+
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+
+        const response = await db.getDb().collection("contacts").replaceOne(
+            { _id: contactId },
+            contact 
+        );
+
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json({ message: "Error updating contact" });
+        } 
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+const deleteContact = async (req, res) => {
+    try {
+        const contactId = new ObjectId(req.params.id);
+
+        const response = await db.getDb().collection("contacts").deleteOne({
+            _id: contactId
+        });
+
+        if (response.deletedCount > 0) {
+            res.status(200).send();
+        } else {
+            res.status(500).json({ message: "Error deleting contact" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact,
 };
